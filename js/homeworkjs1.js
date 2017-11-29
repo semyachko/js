3e3c13ade5d6667105a81187c51e3cdaa4c8ce94
semyachko <funyman309@gmail.com>
2017-11-30 01:56:15 +0300

    homeworkjs1.js

---
+++ js/homeworkjs1.js
@@ -0,0 +1,28 @@
+let a = 23;
+let b = 45;
+let c = 16;
+let z;
+s = 2 * (a * b + a * c + b * c);
+console.log("площадь параллелепипеда " + s);
+
+s = 1000; 
+x = 25;
+y = 15; 
+
+let ploshad = Math.sqrt(s - x * y);
+
+console.log("не занято " + ploshad + "м2");
+
+s_1 = 15;
+s_2 = 600;
+
+s_3 = 15 * 100 - 100;
+
+console.log("площадь овального кольца равна " + s_3);
+
+x = 346
+y = 100
+let vova = 0
+slojna = x / y <= 4 ? vova = x + y : vova = x - y;
+
+console.log("Ответ " + slojna);
